8336f056befd5fc60253a0b0b0257806eea335a9
Edilson Silva Filho <esilva@fesagroup.com>
2022-10-05 07:39:49 -0300

    CRUD with prisma

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { v4 as uuidv4 } from "uuid";
 import cors from "cors"
 import {hash, compare} from "bcrypt"
 import { sign } from 'jsonwebtoken';
+import { prisma } from './database/prismaClient';
+
 
 interface IPropsDB { id: string; name: string; email: string; password:string }[]
 
@@ -23,19 +25,29 @@ const customerDB: IPropsDB[]  = [];
 //Middleware
 
 //Verifica se a conta já existe
-function verifyExistAccount(request:any, response:any, next:any){
+async function verifyExistAccount(request:any, response:any, next:any){
     const {email}:any = request.body
 
-    const customer = customerDB.find(customer => customer.email === email)
+    // const customer = customerDB.find(customer => customer.email === email)
 
-    if(customer){
+    const emailExist = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    })
+
+    console.log(emailExist)
+
+    if(emailExist){
         return response.status(400).json({error: 'Esse e-mail já existe'})
     }
 //para disponibilizar a função para as rotas
-    request.customer = customer
+    request.emailExist = emailExist
 
     return next()
 }
+
+
 
 //Rota para criar a conta
 app.post("/account", verifyExistAccount, async (request, response)=>{
@@ -43,106 +55,139 @@ app.post("/account", verifyExistAccount, async (request, response)=>{
 
     const passwordHash = await hash(password, 8)
 
-    customerDB.push({
-        id:uuidv4(),
-        name,
-        email,
-        password: passwordHash
-    });
+    // customerDB.push({
+    //     id:uuidv4(),
+    //     name,
+    //     email,
+    //     password: passwordHash
+    // });
 
-    return response.status(201).send()
+    const user = await prisma.user.create({
+        data:{
+            name,
+            email,
+            password: passwordHash
+        }
+    })
+    return response.status(201).send(user)
 })
 
 //Rota para ver a conta
-app.get("/show", (request:any,response:any)=> {
+app.get("/show", async (request:any,response:any)=> {
 
-    return response.json(customerDB)
+    const showAll = await prisma.user.findMany({})
+
+    return response.json(showAll)
 })
 
 //Rota pra login, verificador de senha
 app.post("/login", async (request:any ,response:any)=> {
     const { email, password }:any = request.body;
 
-    const dataUser = customerDB.find( data => data.email.includes(email))
+    // const dataUser = customerDB.find( data => data.email.includes(email))
+    const dataUser = await prisma.user.findUnique({
+        where:{
+            email
+        }
+    })
+
+    if(!dataUser){
+        return response.status(400).json({error: 'Esse e-mail ou senha não existe'})
+    }
     const confirmPassword = dataUser?.password
     const passwordMatch = await compare(password, confirmPassword!)
 
-    const token = sign({}, "a1df64cba1f711410b6a4a86942971cb", {
+    if(!passwordMatch){
+        return response.status(400).json({error: 'Esse e-mail ou senha não existe'})
+    }
+
+    const token = sign({
+        user: {
+            name: dataUser?.name,
+            email: dataUser?.email,
+        },
+    }, "a1df64cba1f711410b6a4a86942971cb", {
         subject: dataUser?.id,
         expiresIn: "1d",
+        
     } )
-    
-    const tokenReturn = {
-        token,
-        user: {
-            name: dataUser?.name,
-            email: dataUser?.email,
-        },
-    }
 
-    if(passwordMatch){
-        return response.status(201).json(tokenReturn)
-    } else {
-        return response.status(400).json({error: 'ERRORRRRRRRRRRRR'})
-    }
 
-    
+    return response.status(201).json(token)
+
 })
 
-//Rota pra login, verificador de senha
+//?????????
 app.get("/user", async (request:any ,response:any)=> {
-    const { token }:any = request.header;
+    const { auth }:any = request.headers.authorization;
 
-    const dataUser = customerDB.find( data => data.email.includes(email))
+    const [bearer, token] = auth.split("")
+
+    console.log("tokennnnnnn", token)
+
+    // const dataUser = customerDB.find( data => data.email.includes(email))
     
-    const userReturn = {
-        user: {
-            name: dataUser?.name,
-            email: dataUser?.email,
-        },
-    }
+    // const userReturn = { 
+    //     user: {
+    //         name: dataUser?.name,
+    //         email: dataUser?.email,
+    //     },
+    // }
 
-    if(userReturn){
-        return response.status(201).json(userReturn)
-    } else {
-        return response.status(400).json({error: 'ERRORRRRRRRRRRRR'})
-    }
+    // if(userReturn){
+    //     return response.status(201).json(userReturn)
+    // } else {
+    //     return response.status(400).json({error: 'ERRORRRRRRRRRRRR'})
+    // }
 
     
 })
 
 //Rota pra atualização do nome
-app.put("/account/:email", (request,response)=> {
+app.put("/account/:email", async (request,response)=> {
     //Params pra conseguir pesquisar na rota do put
     const { email } = request.params;
     //Nome que vem do frontend
     const { name } = request.body;
 
     //Procurando o objeto que contem o perfil do usuário pelo e-mail
-    const findEmail = customerDB.find((data) => data.email === email)
+    // const findEmail = customerDB.find((data) => data.email === email)
+    const dataUSer = await prisma.user.update({
+        where:{
+            email
+        },
+        data:{
+           name 
+        }
+    })
 
     //Como e-mail pode ser nulo, então só vai executar se for verdadeiro
-    if(findEmail){
+    if(dataUSer){
         
         //Pegando o nome do banco deixando ele igual ao nome que veio do frontend assim atualizando
-        findEmail.name = name
+        dataUSer.name = name
     }
-    
-    return response.json(customerDB);
+    return response.status(201).json(dataUSer)
+    // return response.json(customerDB);
 });
 
 //Rota para deletar uma conta
-app.delete("/account/:email", (request, response)=>{
+app.delete("/account/:email", async (request, response)=>{
     const {email} = request.params
 
     //descobrindo o index do usuário no array que possui todos os usuários
-    const indexUser = customerDB.map((user:any)=>user.email).indexOf(email)
+    // const indexUser = customerDB.map((user:any)=>user.email).indexOf(email)
 
     //deletando o usuário
-    customerDB.splice(indexUser, 1)
+    const dataUSer = await prisma.user.delete({
+        where:{
+            email
+        },
+    })
+    // customerDB.splice(indexUser, 1)
 
     
-    return response.json(customerDB);
+    return response.status(201).json(dataUSer)
 })
 
 //Porta da aplicação Localjost:3333
